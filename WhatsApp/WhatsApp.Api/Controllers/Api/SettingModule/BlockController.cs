f951d36e473e8a86a27e85ca7983cfab9402ae7e
HeerVaishnav <heervaishnav@gmail.com>
2020-02-25 19:44:03 +0530

    web(code):added setting module

---
+++ WhatsApp/WhatsApp.Api/Controllers/Api/SettingModule/BlockController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WhatsApp.Domain.SettingModule;
+using WhatsApp.Models.Main;
+using RxWeb.Core.AspNetCore;
+using RxWeb.Core.Security.Authorization;
+using WhatsApp.Models.ViewModels;
+
+
+namespace WhatsApp.Api.Controllers.SettingModule
+{
+    [ApiController]
+    [Route("api/[controller]")]
+	
+	public class BlockController : BaseDomainController<BlockedNumber,BlockedNumber>
+
+    {
+        public BlockController(IBlockDomain domain):base(domain) {}
+
+    }
+}
---
+++ WhatsApp/WhatsApp.Models/ViewModels/BlockedNumber.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WhatsApp.Models.Main;
+
+namespace WhatsApp.Models.ViewModels
+{
+     public class BlockedNumber : MyContact
+    {
+        public int BlockedUserId { get; set; }
+
+        public new int UserId { get; set; }
+
+    }
+}
